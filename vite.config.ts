c68a1635d5133df81d031e2ecc9536220b10306c
Mason <mason.d.mcmanus@outlook.com>
2025-09-01 23:03:50 -0500

    Fix build function

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: '/TrackerMLB/',
-  build: {
-    outDir: "build/client" // make sure it aligns with Remix/React Router’s client build
-  }
 });
